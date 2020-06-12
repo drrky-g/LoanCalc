86d3c97b5fa73c20aa137b19df15c120573062f9
Derrick Gordon <derrickwg17@gmail.com>
2020-06-12 04:36:31 -0700

    added api example page w/ consumer script

--- LoanCalc/Controllers/LoanController.cs
+++ LoanCalc/Controllers/LoanController.cs
@@ -5,42 +5,25 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace LoanCalc.Controllers
-{
-    public class LoanController : Controller
-    {
-        // GET: Loan
+namespace LoanCalc.Controllers {
+    public class LoanController : Controller {
+        // Main App
         public ActionResult Index() { return View(); }
+        // Api Example
+        public ActionResult Example() { return View(); }
+        // Table Partial
         public ActionResult Calculate(LoanParams loan) {
             LoanTable table = new LoanTable(loan);
-
-            double amount = Math.Round(loan.Principal,2);
-            double rate = loan.InterestRate / 100;
-            double payment = Math.Round((amount * (rate / 12) * (Math.Pow(1 + rate / 12, loan.TermLengthMonths))) / (Math.Pow(1 + (rate / 12), loan.TermLengthMonths) - 1), 2);
-
-            double startingBalance = amount;
-            double endingBalance = amount;
-            double totalInterestPaid = 0.00;
-
-            for(int month = 1; month <= loan.TermLengthMonths; month++) {
-                double interestCharge = Math.Round(rate / 12 * startingBalance, 2);
-                totalInterestPaid += interestCharge;
-                endingBalance = Math.Round(startingBalance + interestCharge - payment,2);
-                if(endingBalance < 1 &&
-                    endingBalance > 0) {
-                    endingBalance = 0.00;
-                }
-                table.TableData.Add(new PaymentSummary {
-                    Month = month,
-                    PaymentAmount = payment,
-                    PrinciplePaymentAmount = payment - interestCharge,
-                    InterestPaymentAmount = interestCharge,
-                    TotalInterestPaid = totalInterestPaid,
-                    RemainingBalance = endingBalance
-                });
-                startingBalance = endingBalance;
-            }
             return PartialView(table);
+        }
+        // Json Endpoint
+        public JsonResult AsJson(int amount, int months, int rate) {
+            LoanParams loan = new LoanParams {
+                InterestRate = rate,
+                Principal = amount,
+                TermLengthMonths = months
+            };
+            return Json(new LoanTable(loan), JsonRequestBehavior.AllowGet);
         }
     }
 }
